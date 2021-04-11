3ca41fd602e6e145bf76a269ef5fa32ec5430f61
Inessa Vasilevskaya <ivasilev@redhat.com>
2021-04-11 23:07:29 +0200

    Count VZP expenses as well for all groups of foreigners (not only children)

--- README.md
+++ README.md
@@ -8,12 +8,36 @@ In a python 3.6+ virtualenv:
 1. Install requirements with `pip install -r requirements.txt`
 2. Run the script with `python count_children.py priloha_882435642_1_TDU_věk_2019-2020.xlsx`
 
-The output format might change slightly, but should be similar to:
+The script will produce an out.csv with the following information: foreigners (visa over 90 days and long-term residence)
+numbers for different age groups and VZP expenses for those groups in year 2019.
+
+## Additional information
+
+Futher calculations, better representation and diagrams can be found at 
+[The 2019 foreigners VZP contributions&expenses analysis](https://docs.google.com/spreadsheets/d/1k0ynJQ6F30v-cjznDTgPboe1Ug31DsXN6PRjgQXilw8).
+
+Comments and reviews are welcome!
+
 ```
-Stats for age group 0 - 18:
-Children (visa over 90 days and long-term residence) from non-EU countries born 2002 - 2019 by 31/12/2019 - 11581
-Children (visa over 90 days and long-term residence) from non-EU countries born 2003 - 2020 by 31/12/2020 - 14066
-Stats for age group 0 - 15:
-Children (visa over 90 days and long-term residence) from non-EU countries born 2005 - 2019 by 31/12/2019 - 9466
-Children (visa over 90 days and long-term residence) from non-EU countries born 2006 - 2020 by 31/12/2020 - 11330
+Age group,Avg cost per person,Total foreigners,Total expenses (mln czk)
+0-4,21084,3571,75.290964
+5-9,11209.5,3369,37.7648055
+10-14,12322,2526,31.125372
+15-19,13647.5,7573,103.3525175
+20-24,12600,22780,287.028
+25-29,15192.5,28269,429.4767825
+30-34,17076,25265,431.42514
+35-39,17460,20690,361.2474
+40-44,18488.5,16502,305.097227
+45-49,21865.5,13168,287.924904
+50-54,26685.5,9599,256.1541145
+55-59,32562,6823,222.170526
+60-64,40524.5,3468,140.538966
+65-69,50573.5,2004,101.349294
+70-74,61911.5,1343,83.1471445
+75-79,72105,879,63.380295
+80-84,76047,419,31.863693
+85-89,82282.5,177,14.5640025
+90-94,85478,76,6.496328
+95+,90975,29,2.638275
 ```
--- count_children.py
+++ count_children.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 
 import pandas as pd
@@ -9,6 +10,38 @@ EU_COUNTRIES = [
         "Litva", "Španělsko", "Švédsko", "Maďarsko", "Malta", "	Německo", "Nizozemsko", "Polsko", "Portugalsko",
         "Rakousko", "Rumunsko", "Řecko", "Slovensko", "Slovinsko", "Lotyšsko", "Lucembursko"
 ]
+ADULT_AGE = 18
+# According to Výsledky zdravotnických účtů ČR - 2010–2019
+# https://www.czso.cz/csu/czso/vysledky-zdravotnickych-uctu-cr-m6hwrlzbbw ,
+# average numbers calculated
+EXPENSES_PER_AGE_GROUP = {
+        # economically inactive: children
+        (0, 4): 21084,
+        (5, 9): 11209.5,
+        (10, 14): 12322,
+        (15, 18): 13647.5,  # approximation, will take same value as (15, 19)
+        (15, 19): 13647.5,
+        # economically active
+        (20, 24): 12600,
+        (25, 29): 15192.5,
+        (30, 34): 17076,
+        (35, 39): 17460,
+        (40, 44): 18488.5,
+        (45, 49): 21865.5,
+        (50, 54): 26685.5,
+        (55, 59): 32562,
+        (60, 64): 40524.5,
+        # economically inactive: retired
+        (65, 69): 50573.5,
+        (70, 74): 61911.5,
+        (75, 79): 72105,
+        (80, 84): 76047,
+        (85, 89): 82282.5,
+        (90, 94): 85478,
+        (95, None): 90975  # all people over 95+ fall under this category
+}
+PREMIUM_OBZP_2019 = 1803 * 12
+OUT = 'out.csv'
 
 
 class ProcessError(Exception):
@@ -16,7 +49,7 @@ class ProcessError(Exception):
 
 
 # Magic number explanation: Line 547 is the line with years mapping information, 100 is the column for 2002
-def _get_years(df, years_row=547, first_col=100, last_col=-1):
+def _get_years(df, years_row=547, first_col=3, last_col=-1):
     years = df.iloc[years_row:years_row+1, first_col:last_col].to_dict()
     res = {}
     for col_name, value_dict in years.items():
@@ -24,12 +57,14 @@ def _get_years(df, years_row=547, first_col=100, last_col=-1):
     return res
 
 
-def do_count(df, first_row, last_row, year, max_age=18):
+def do_count(df, first_row, last_row, year, max_age=ADULT_AGE, min_age=0):
     countries = [c for c in df[COLUMN][first_row:last_row].to_list()
                  if isinstance(c, str) and not c.endswith('Celkem')]
     years = _get_years(df)
 
-    children_columns = [c for y, c in years.items() if y in range(year-max_age+1,year+1)]
+    r1 = year - max_age + 1
+    r2 = year - min_age + 1
+    children_columns = [c for y, c in years.items() if y in range(r1, r2)]
     children_long_term_residence = {}
     # XXX FIXME there should be a fancy way to keep country name together with the data
     sums = df.iloc[first_row:last_row,:].loc[df[COLUMN].isin(countries), children_columns].sum(axis=1).to_list()
@@ -38,8 +73,9 @@ def do_count(df, first_row, last_row, year, max_age=18):
     # filter out EU countries
     non_eu = [c for c in countries if c not in EU_COUNTRIES]
     sum_non_eu = sum(children_long_term_residence[c] for c in countries if c in non_eu)
-    print(f"Children (visa over 90 days and long-term residence) from non-EU countries "
-          f"born {year - max_age + 1} - {year} by 31/12/{year} - {sum_non_eu}")
+    print(f"Foreigners (visa over 90 days and long-term residence) from non-EU countries "
+          f"born {r1} - {r2 - 1} by 31/12/{year} - {sum_non_eu}")
+    return sum_non_eu
 
 
 def main(args=sys.argv[1:]):
@@ -47,12 +83,31 @@ def main(args=sys.argv[1:]):
         raise ProcessError("Please pass an xlsx file as first argument")
     xl_file = pd.read_excel(args[0], sheet_name=None)
     df = xl_file['pobyt_31.12.2019-2020']
-    print("Stats for age group 0 - 18:")
-    do_count(df, 5, 542, 2019)
-    do_count(df, 548, -1, 2020)
-    print("Stats for age group 0 - 15:")
-    do_count(df, 5, 542, 2019, 15)
-    do_count(df, 548, -1, 2020, 15)
+    total_expenses = 0
+    total_premiums = 0
+    year = 2019
+    first_row_map = {2019: 5, 2020: 548}
+    last_row_map = {2019: 542, 2020: -1}
+    age_ranges = [(r, r+5) for r in range(0, 95, 5)] + [(95, None)]
+    with open(OUT, 'w', newline='') as csvfile:
+        fieldnames = ['Age group', 'Avg cost per person', 'Total foreigners', 'Total expenses (mln czk)']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i, (r1, r2) in enumerate(age_ranges):
+            if r1 == 95:
+                group_size = do_count(df, first_row_map[year], last_row_map[year], year, max_age=142, min_age=r1)
+                avg_price = EXPENSES_PER_AGE_GROUP[(95, None)]
+                age_group = '95+'
+            else:
+                group_size = do_count(df, first_row_map[year], last_row_map[year], year, max_age=r2, min_age=r1)
+                avg_price = EXPENSES_PER_AGE_GROUP[(r1, r2 - 1)]
+                age_group = f'{r1}-{r2-1}'
+            group_expenses = avg_price * group_size
+            writer.writerow({'Age group': age_group,
+                             'Total expenses (mln czk)': group_expenses/10**6,
+                             'Total foreigners': group_size,
+                             'Avg cost per person': avg_price})
+
 
 if __name__ == '__main__':
     main()
